0ffc4290be9d69decefff377e7c87d2c388778db
ShaYri12 <sharjeelb60@gmail.com>
2024-09-12 14:20:05 +0500

    last section added

--- app/components/MyRecentProjects.js
+++ app/components/MyRecentProjects.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
 
 const MyRecentProjects = () => {
   return (
-    <div>MyRecentProjects</div>
-  )
-}
+    <section className="bg-white px-4">
+      <div className="md:py-[100px] py-[50px] max-w-[1280px] mx-auto overflow-hidden">
+        {/* About Text Section */}
+        <div id="projects" className="mb-[50px] pt-[20px]">
+          <h2 className="uppercase text-[22.46px] leading-[26.95px] font-[400] uppercase text-[#6D7D7D] tracking-[6px] text-center">
+            Project
+          </h2>
+          <h1 className="sm:text-[56px] text-[40px] sm:leading-[67.37px] leading-[50px] font-[600] text-primaryColor mb-[23px] mt-[6px] text-center">
+            My Recent Projects
+          </h1>
+          <p className="max-w-[1048px] mx-auto text-secondaryColor md:text-[20px] text-[19px] font-[400] leading-[30px] text-center">
+            Recently completed a modern home renovation that seamlessly blends
+            contemporary aesthetics with functional living spaces, reflecting
+            the client’s unique style.
+          </p>
+        </div>
+        <div className="">
+          <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            {/* Row 1 */}
+            <div className="sm:col-span-1 xl:h-[400px] sm:h-[320px] w-full">
+              <img
+                src="/assets/recent-project1.png"
+                alt="Image 1"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            <div className="sm:col-span-2 xl:h-[400px] h-[320px]">
+              <img
+                src="/assets/recent-project2.png"
+                alt="Image 2"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            <div className="sm:col-span-1 xl:h-[400px] h-[320px]">
+              <img
+                src="/assets/recent-project3.png"
+                alt="Image 3"
+                className="object-cover w-full h-full"
+              />
+            </div>
 
-export default MyRecentProjects
+            {/* Row 2 */}
+            <div className="sm:col-span-2 xl:h-[400px] h-[320px]">
+              <img
+                src="/assets/recent-project4.png"
+                alt="Image 4"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            <div className="sm:col-span-1 xl:h-[400px] h-[320px]">
+              <img
+                src="/assets/recent-project5.png"
+                alt="Image 5"
+                className="object-cover w-full h-full"
+              />
+            </div>
+            <div className="sm:col-span-1 xl:h-[400px] h-[320px]">
+              <img
+                src="/assets/recent-project6.png"
+                alt="Image 6"
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyRecentProjects;
--- app/components/Process.js
+++ app/components/Process.js
@@ -6,39 +6,39 @@ const Process = () => {
     <section className="bg-color-bg md:px-0 px-4">
       <div className="md:py-[160px] py-[60px] overflow-hidden">
         {/* About Text Section */}
-        <div className="mb-[50px] pt-[20px]">
+        <div className="mb-[50px] pt-[20px] md:px-4">
           <h2 className="uppercase text-[22.46px] leading-[26.95px] font-[400] uppercase text-[#6D7D7D] tracking-[6px] text-center">
             PROCESS
           </h2>
           <h1 className="sm:text-[56px] text-[40px] sm:leading-[67.37px] leading-[50px] font-[600] text-primaryColor mb-[23px] mt-[6px] text-center">
             My Working Approach
           </h1>
-          <p className="max-w-[1011px] mx-auto text-secondaryColor md:text-[20px] text-[19px] font-[400] leading-[30px] text-center">
+          <p className="max-w-[1048px] mx-auto text-secondaryColor md:text-[20px] text-[19px] font-[400] leading-[30px] text-center">
             I maintain open communication, providing regular updates and
             involving you in key decisions. This collaborative approach ensures
             that the final design is not only visually stunning but also
             perfectly aligned with your expectations.
           </p>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-between gap-12 md:gap-4 lg:gap-14 py-12 lg:ps-[100px] ps-4">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-16 sm:gap-20 md:gap-4 lg:gap-14 md:py-12 xl:ps-[100px] lg:ps-[50px] md:ps-4">
           {/* Text Section */}
           <div className="md:w-1/2">
-            <div className="mb-12">
-              <h3 className="text-2xl font-bold text-gray-900">
+            <div className="md:mb-[100px] mb-[50px]">
+              <h3 className="lg:text-[36px] md:text-[30px] sm:text-[26px] text-[23px] font-[600] text-primaryColor leading-[48px]">
                 Step 01: Understanding
               </h3>
-              <p className="text-gray-600 mt-4">
+              <p className="text-secondaryColor lg:text-[20px] sm:text-[18px] text-[17px] font-[400] leading-[30px] md:mt-[20px] mt-[10px]">
                 I start by engaging with you to fully understand your needs,
                 preferences, and lifestyle. This involves detailed consultations
                 where we discuss your vision, the functionality of the space,
                 and any specific requirements you have.
               </p>
             </div>
-            <div className="mb-12">
-              <h3 className="text-2xl font-bold text-gray-900">
+            <div className="md:mb-[100px] mb-[50px]">
+              <h3 className="lg:text-[36px] md:text-[30px] sm:text-[26px] text-[23px] font-[600] text-primaryColor leading-[48px]">
                 Step 02: Conceptualization
               </h3>
-              <p className="text-gray-600 mt-4">
+              <p className="text-secondaryColor lg:text-[20px] sm:text-[18px] text-[17px] font-[400] leading-[30px] md:mt-[20px] mt-[10px]">
                 Based on our discussions, I create initial design concepts and
                 draft plans. This phase focuses on blending aesthetics with
                 functionality, ensuring that each design element serves a
@@ -46,10 +46,10 @@ const Process = () => {
               </p>
             </div>
             <div>
-              <h3 className="text-2xl font-bold text-gray-900">
+              <h3 className="lg:text-[36px] md:text-[30px] sm:text-[26px] text-[23px] font-[600] text-primaryColor leading-[48px]">
                 Step 03: Execution
               </h3>
-              <p className="text-gray-600 mt-4">
+              <p className="text-secondaryColor lg:text-[20px] sm:text-[18px] text-[17px] font-[400] leading-[30px] md:mt-[20px] mt-[10px]">
                 Throughout the project, I maintain clear communication and
                 involve you in key decisions. This collaborative process ensures
                 that the final design aligns perfectly with your expectations. I
@@ -68,7 +68,7 @@ const Process = () => {
             />
             <button className="absolute inset-0 flex items-center justify-center z-[3]">
               <Image
-                className="pointer"
+                className="pointer md:w-[162px] w-[120px]"
                 width={162}
                 height={162}
                 src="/assets/play-button.svg"
